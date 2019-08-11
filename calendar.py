a648d092ed01262fa4d1463a5fab353449998f07
Joe <lumtwj@gmail.com>
2019-08-11 11:25:13 +0800

    Added unique id for each holidays and allows loading of previous year calendar

--- calendar.py
+++ calendar.py
@@ -2,14 +2,32 @@ import json
 
 
 class Calendar:
-    def __init__(self, raw, year, calendar_version):
+    def __init__(self, raw, year, ):
         self.raw = raw
         self.year = year
-        self.calendar_version = calendar_version
+        self.calendar_version = 1
         self.version = None
         self.calendar_name = None
         self.timezone = None
         self.events = []
+        self.last_unique_index = 0
+
+    def load_json(self, json_calendar):
+        with open(json_calendar, 'r') as f:
+            calendar = json.load(f)
+            calendar_year = calendar['year']
+            public_holidays = calendar['data']
+
+            if self.year > calendar_year:
+                for public_holiday in public_holidays:
+                    date = public_holiday['date']
+
+                    if calendar_year == int(date[0:4]):
+                        print(public_holiday)
+                        self.events.append(public_holiday)
+
+                self.last_unique_index = public_holidays[len(public_holidays) - 1]['uid']
+                self.calendar_version += 1
 
     def parse(self):
         name = None
@@ -29,7 +47,9 @@ class Calendar:
             elif line.startswith("DTSTART;VALUE=DATE:"):
                 date = line[line.index(':') + 1:]
             elif line == "END:VEVENT":
+                self.last_unique_index += 1
                 self.events.append({
+                    "uid": self.last_unique_index,
                     "name": name,
                     "date": date
                 })
--- calendar_parser.py
+++ calendar_parser.py
@@ -1,11 +1,11 @@
 import requests
-from Calendar import Calendar
+from calendar import Calendar
 
-url = 'https://www.mom.gov.sg/~/media/mom/documents/employment-practices/public-holidays/public-holidays-sg-2019.ics'
-calendar = Calendar(requests.get(url).text, 2019, 1)
+url = 'https://www.mom.gov.sg/~/media/mom/documents/employment-practices/public-holidays/public-holidays-sg-2020.ics'
+calendar = Calendar(requests.get(url).text, 2020)
 
+calendar.load_json('./json/public_holidays_sg.json')
 events = calendar.parse()
-# calendar_name = calendar.calendar_name.replace(' ', '_').lower()
 
 f = open('./json/public_holidays_sg.json', "w")
 f.write(events)

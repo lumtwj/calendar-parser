b0dd15609885d9134ec32da26a266730fa897b82
Joe Lum <lumtwj@gmail.com>
2020-11-28 23:57:47 +0800

    Updated calendar for 2021 public holiday

--- calendar_parser.py
+++ calendar_parser.py
@@ -1,8 +1,8 @@
 import requests
 from ics_calendar import Calendar
 
-url = 'https://www.mom.gov.sg/~/media/mom/documents/employment-practices/public-holidays/public-holidays-sg-2020.ics'
-calendar = Calendar(requests.get(url).text, 2020)
+url = 'https://www.mom.gov.sg/~/media/mom/documents/employment-practices/public-holidays/public-holidays-sg-2021.ics'
+calendar = Calendar(requests.get(url).text, 2021)
 
 calendar.load_json('./json/public_holidays_sg.json')
 events = calendar.parse()

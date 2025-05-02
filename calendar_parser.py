b2174677b0e27502fd67ee058dbb86e01e71ea2b
Joe Lum <lumtwj@gmail.com>
2025-05-02 12:48:14 +0800

    Dynamically retrieve year instead of hardcode

--- calendar_parser.py
+++ calendar_parser.py
@@ -1,8 +1,14 @@
 from ics_calendar import Calendar
+import datetime
+from zoneinfo import ZoneInfo
+
+now = datetime.datetime.now(ZoneInfo('Asia/Singapore'))
+year = now.year
+print('Current year:', year)
 
 public_holiday_json = './json/public_holidays_sg.json'
 
-calendar = Calendar(2025)
+calendar = Calendar(year)
 calendar.load_json(public_holiday_json)
 events = calendar.parse()
 
